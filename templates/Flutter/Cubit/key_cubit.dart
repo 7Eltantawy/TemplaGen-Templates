0d9c58a343181a02791be58987a5acd783cca7d3
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2023-03-09 05:03:54 +0200

    Flutter Cubit Template

---
+++ templates/Flutter/Cubit/key_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part '<{key_sc}>_state.dart';
+
+class <{key}>Cubit extends Cubit<<{key}>State> {
+  <{key}>Cubit() : super(<{key}>Initial());
+}
---
+++ templates/Flutter/Cubit/key_state.dart
@@ -0,0 +1,6 @@
+part of '<{key_sc}>_cubit.dart';
+
+@immutable
+abstract class <{key}>State {}
+
+class <{key}>Initial extends <{key}>State {}
